ee0b3d6ae308db679515fd7a1e2ee9bf370614dc
JulioSulub1897 <jcsulub@gmail.com>
2017-06-27 10:25:41 -0500

    Clase de Figuras
    class triangulo

---
+++ Bootstrap/figura.class.php
@@ -0,0 +1,35 @@
+<?php
+class figura{
+	public $nombre;
+	public $color;
+	
+	function __construct(){
+		$this->nombre="";
+		$this->color="";
+	}
+}
+class triangulo extends figura{
+	public $base;
+	public $altura;
+	
+	function __construct(){
+		$this->base=0;
+		$this->altura=0;
+	}
+		
+	function area(){
+		return $this->base*$this->altura/2;	
+	}
+	function inicializar ($n,$c,$base,$altura){
+		$this->nombre=$n;
+		$this->color=$c;
+		$this->base=$base;
+		$this->altura=$altura;
+	}
+}
+
+$figura= new triangulo();
+$figura->inicializar("triangulito","CCA326",20,10);
+echo $figura->area();
+
+?>
